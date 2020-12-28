bc8f154fc08f85f5a7d5ff0cf1e4ed47fe8d3a79
jldelrioluque <76702924+jldelrioluque@users.noreply.github.com>
2020-12-28 20:58:28 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ basic.forever(function () {
     if (maqueen.sensor(PingUnit.Centimeters) < 30 && maqueen.sensor(PingUnit.Centimeters) != 0) {
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 255)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(300)
         item = Math.randomBoolean()
         if (item == true) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
